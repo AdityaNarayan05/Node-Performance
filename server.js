de514ef2dcf645005c5fd0adaea413546ff11992
Aditya Narayan <nlaxmi886@gmail.com>
2022-09-16 15:49:00 +0530

    Server modiefied for clustering

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cluster = require('cluster');
+const os = require('os');
 
 const app = express();
 
@@ -10,14 +12,25 @@ function delay(duration) {
 }
 
 app.get('/', (req, res) => {
+  // JSON.stringify({}) => "{}"
+  // JSON.parse("{}") => {}
+  // [5,1,2,3,4].sort()
   res.send(`Performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
-  delay(4000);
-  res.send(`Beep beep beep! ${process.pid}`);
+  delay(9000);
+  res.send(`Ding ding ding! ${process.pid}`);
 });
 
 console.log('Running server.js...');
-console.log('Worker process started.');
-app.listen(3000);
+if (cluster.isMaster) {
+  console.log('Master has been started...');
+  const NUM_WORKERS = os.cpus().length;
+  for (let i = 0; i < NUM_WORKERS; i++) {
+    cluster.fork();
+  }
+} else {
+  console.log('Worker process started.');
+  app.listen(3000);
+}
